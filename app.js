dbc340be47659116f0089e9b619ed12fbb498339
saranrajmcse <saranrajmcse@gmail.com>
2025-01-28 11:24:12 +0530

    update

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
   .then(() => {
     console.log('Connected to MongoDB');
     // Start the server
-    app.listen(3000, () => {
+    app.listen(process.env.PORT, () => {
       console.log(`Server is running on http://localhost:${process.env.PORT}`);
     });
   })
